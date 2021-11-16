9e54c544744e761059a48e283d2c6c7a537aec64
Michael Fisher <mfisher@kushview.net>
2021-11-16 11:09:15 -0500

    Updave vscode files

--- .vscode/vscode.h
+++ .vscode/vscode.h
@@ -1,5 +1,13 @@
 #define JUCE_AMALGAMATED_INCLUDE 1
 #define KV_JACK_AUDIO 0
+
+#include "lua.h"
+
+#ifndef EL_PRO
+#define EL_PRO 1
+#endif
+
+#ifdef __cplusplus
 #include "JuceHeader.h"
 #include "ElementApp.h"
 #include "Globals.h"
@@ -15,3 +23,4 @@ using PortDescription = kv::PortDescription;
 using PortType = kv::PortType;
 using PortList = kv::PortList;
 }
+#endif
