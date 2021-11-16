b8d9f4843ed6e5ff42465ce7f4bab0b80fe82e9d
Michael Fisher <mfisher@kushview.net>
2021-11-16 11:45:22 -0500

    Add playground internal loadable plugins/modules

---
+++ modules/jack.element/jack.c
@@ -0,0 +1,29 @@
+
+#include <element/plugin.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct JackPlugin {
+} JackPlugin;
+
+static elHandle el_jack_create(const elFeature* const* params)
+{
+    (void)params;
+    JackPlugin* jack = (JackPlugin*)malloc(sizeof(JackPlugin));
+    return jack;
+}
+
+static void el_jack_destroy(elHandle handle)
+{
+    JackPlugin* jack = (JackPlugin*)handle;
+    free(jack);
+    printf("JACK destroyed\n");
+}
+
+const elDescriptor* eds_descriptor()
+{
+    static const struct elDescriptor D = {
+        .create = el_jack_create,
+        .destroy = el_jack_destroy };
+    return &D;
+}
---
+++ modules/jack.element/manifest.lua
@@ -0,0 +1,13 @@
+--- Stereo Amplifier in Lua.
+--
+-- The code contained implements a simple stereo amplifier plugin. It does not
+-- try to smooth the volume parameter and could cause zipper noise.
+--
+-- @module      amp
+-- @kind        DSP
+-- @license     GPL v3
+-- @author      Michael Fisher
+
+name   = "JACK"
+author = "Michael Fisher"
+email  = ""
